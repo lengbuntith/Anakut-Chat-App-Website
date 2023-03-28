c4c084abab4dc98a3269f1864b89c7394bfd7a53
lengbuntith <lengbuntith2000@gmail.com>
2023-03-28 09:04:44 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="Chat App"
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    >
+      <template #left>
+        <van-search v-model="value" shape="round" placeholder="Placeholder" />
+      </template>
+
+      <template #right>
+        <van-icon name="setting-o" />
+      </template>
+    </van-nav-bar>
+
+    <van-tree-select
+      :items="items"
+      :main-active-index.sync="active"
+      height="100vh"
+    >
+      <template #content>
+        <div v-if="active == 0">
+          <div style="padding: 20px">
+            <div
+              v-for="(c, i) in items[active].chat"
+              :key="i"
+              style="display: flex"
+              :style="
+                c.user == currentUser
+                  ? ' justify-content: end'
+                  : 'justify-content: start'
+              "
+            >
+              <div
+                style="
+                  border: 1px solid #eee;
+                  padding: 10px;
+                  border-radius: 10px;
+                  width: max-content;
+                  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+                "
+              >
+                {{ c.message }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="active == 1"></div>
+      </template>
+    </van-tree-select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: 'Kirito',
+          badge: 2,
+          chat: [
+            {
+              user: 'tith',
+              message: 'Hello',
+            },
+
+            {
+              user: 'kirito',
+              message: 'Yes HI',
+            },
+          ],
+        },
+        { text: 'Asuna', badge: '', chat: [] },
+        { text: 'Sinon', badge: '', chat: [] },
+      ],
+      active: 0,
+      currentUser: 'tith',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    "
+  >
+    <van-form @submit="onSubmit" style="max-width: 400px">
+      <h2 style="text-align: center">Login Account</h2>
+      <van-field
+        v-model="username"
+        name="Username"
+        label="Username"
+        placeholder="Username"
+        :rules="[{ required: true, message: 'Username is required' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="Password"
+        label="Password"
+        placeholder="Password"
+        :rules="[{ required: true, message: 'Password is required' }]"
+      />
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit">
+          Submit
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
