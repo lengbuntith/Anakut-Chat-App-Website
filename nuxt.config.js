2cebe12bc245efebe49facbcc3328e91240078c5
lengbuntith <lengbuntith2000@gmail.com>
2023-03-28 13:14:04 +0700

    using yarn

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   css: ['vant/lib/index.css', '~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/vant'],
+  plugins: [
+    '@/plugins/vant',    
+    { src: '~/plugins/vuex-persist', ssr: false }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
